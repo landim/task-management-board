acf24d398ec70fc4bedeedc73053d48821af7794
Arthur Landim <landim.arthur@gmail.com>
2020-11-17 19:16:29 -0300

    wip: add new card

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,22 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskCard from './TaskCard';
 import './Column.css';
 
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     padding: theme.spacing(1),
-//     textAlign: 'center',
-//     background: '#bbb',
-//   },
-//   title: {
-//     fontSize: '1.5em',
-//     fontWeight: 400,
-//     padding: theme.spacing(1)
-//   }
-// }));
-
 const Column = ({title, columnId, tasks}) => {
   const columnTasks = tasks.filter(task => task.columnId === columnId);
+
+  const [showNewCard, toggleNewCard] = useState(false);
+
+  const cancelNewCard = () => {
+    toggleNewCard(false);
+  }
 
   return (
     <div className="column">
@@ -24,6 +17,10 @@ const Column = ({title, columnId, tasks}) => {
       {columnTasks.map(task => (
         <TaskCard title={task.title} />
       ))}
+      {showNewCard && (
+        <TaskCard columnId={columnId} cancelNewCardCallback={cancelNewCard} />
+      )}
+      <div className={classes.addCard} onClick={() => toggleNewCard(true)}>+ Add another card</div>
     </div>
   );
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskCard.css';
 
 const TaskCard = ({title}) => {
+  const [editing, setEditing] = useState(!task);
+  const [taskTitle, setTaskTitle] = useState(task && task.title);
+
+  const exitCardEditing = () => {
+    setEditing(false);
+    if (!task) {
+      cancelNewCardCallback();
+    }
+  }
+
   return (
     <div className="card">
-      <div className="title">{title}</div>
+      {editing ? (
+        <div>
+          <div className={classes.modalOuter} />
+          <div 
+          className={classes.editTaskModal}
+          // onBlur={exitCardEditing}
+          >
+            <TextField
+              id="new-card-text"
+              className={classes.editingTaskTitle}
+              label="My task"
+              multiline
+              rows={3}
+              value={taskTitle}
+              onChange={() => setTaskTitle(taskTitle)}
+            />
+
+          </div>
+        </div>
+      ) : (
+        <div className="title">{title}</div>
+      )}
     </div>
   );
 }
